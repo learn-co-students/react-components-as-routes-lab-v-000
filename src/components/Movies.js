ee7f7f229feb88799ec2dc20875edf444f28c788
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-05-02 22:25:47 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => (
+        <div key={idx}>
+          <h3>Title: {movie.title}</h3>
+          <h3>Runtime: {movie.time}</h3>
+          <p>Movies:</p>
+            <ul>
+              {movie.genres.map((genre, idx) => (
+                <li key={idx}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
