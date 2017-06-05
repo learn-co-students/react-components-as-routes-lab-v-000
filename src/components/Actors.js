708b540d2cf88bfaa60ec042af0b506b0f36ea74
Scott Savage <snsavage@gmail.com>
2017-06-05 14:12:16 -0400

    Pass all lab specs

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (<div />);
+  return(
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((e, i) =>
+        <div key={i}>
+          <p>{e.name}</p>
+          <p>Movies:{e.movies.map((e, i) =>
+            ` ${e}`
+          )}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((e, i) =>
+        <div key={i}>
+          <p>{e.title} - Time: {e.time}</p>
+          <p>Metascore: {e.metascore}</p>
+          <p>Genres:{e.genres.map((e, i) =>
+            ` ${e}`
+          )}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Movies;
