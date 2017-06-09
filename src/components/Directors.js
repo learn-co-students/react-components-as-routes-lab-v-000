4a62c2a67bd5629c0ef76211f62f9733933da657
github username <jilustrisimo@users.noreply.github.com>
2017-06-09 13:36:32 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      { directors.map((dir, idx) => 
+        <div key={ idx }>
+          <h2>{ dir.name }</h2>
+          <ul>
+            { dir.movies.map((mov, idx) => 
+              <li key={ idx }>{ mov }</li>
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      { movies.map((mov, idx) => 
+        <div key={ idx }>
+          <h2>{ mov.title }</h2>
+          <h3>Length: { mov.time }</h3>
+          <ul>
+          { mov.genres.map((gnr, idx) => 
+            <li key={ idx }>{ gnr }</li>
+          )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Movies;
