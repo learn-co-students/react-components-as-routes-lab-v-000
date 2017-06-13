6e4ad1ae7a0c4143b4b507ca32699faa2d54535b
github username <krishel.lasam@gmail.com>
2017-06-13 01:08:14 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => 
+        <div key={index}>
+          Name: {movie.title}
+          Time: {movie.time}
+          <ul>
+            Genres: {movie.genres.map((genre, index) => 
+              <li key={index}>{genre}</li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
