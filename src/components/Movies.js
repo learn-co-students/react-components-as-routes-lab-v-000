0deda9bf6bfcafea1fdd44a31fcc0abf7dba2b91
github username <kevinladkins@gmail.com>
2017-06-19 15:06:53 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => {
+        return (
+          <div>
+            <h2>{movie.title}</h2>
+            Time: {movie.time} <br />
+            Genres:
+            <ul>
+              {movie.genres.map((genre, index)=> {
+                return (<li key={index}>{genre}</li>)
+              }
+                )
+              }
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
