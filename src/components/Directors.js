fa3b353fa33a54e9450adab0896decf50eb154d4
Phreemason <github email address>
2017-06-27 12:13:57 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+	const directorShow = directors.map(director=>{
+		return (
+			<div>
+			  <h4>Name: {director.name}</h4>
+			  <p>Movies:</p> <ul>{director.movies.map(movie=>(<li>{movie}</li>))}</ul> 
+		  </div>
+		  )
+	})
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Directors Page</h1>
+      {directorShow}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+	const movieShow = movies.map(movie=>{
+		return (
+			<div>
+			  <h4>Name: {movie.title}</h4>
+        <p> Time: {movie.time} </p>
+			  <p>Genres:</p> <ul>{movie.genres.map(genre=>(<li>{genre}</li>))}</ul> 
+		  </div>
+		  )
+	})
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+        {movieShow}
     </div>
   );
 };
