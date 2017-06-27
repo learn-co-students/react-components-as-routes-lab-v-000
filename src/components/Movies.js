bfa14c3e0c093a5bafce167a5bb47595933136fa
bentonwong <bentonwong@gmail.com>
2017-06-26 18:19:23 -0700

    done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index)=>(
+          <div key={index}>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <p>Genres: </p>
+            <ul>{movie.genres.map((genre, genre_index)=>(
+              <li>{genre}</li>
+            ))}</ul>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Movies;
+
+//This component should render the text Movies Page, and make a new <div> for each movie. The <div> should contain the movie's title, time and an <ul> for each genre.
