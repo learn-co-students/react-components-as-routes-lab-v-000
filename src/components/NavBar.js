3a77b4c6b1a6cdbca36d3985460296f9c4b789c9
DanL12186 <danlrcn@hotmail.com>
2017-07-29 12:11:29 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+    <NavLink
+      to="/"
+      /* set exact so it knows only to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Home</NavLink>
+    <NavLink
+      to="/movies"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'black'
+      }}
+      >Movies</NavLink>
+    <NavLink
+      to="/actors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Actors</NavLink>
+    <NavLink
+      to="/directors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+      >Directors</NavLink>
     </div>
   );
 };
