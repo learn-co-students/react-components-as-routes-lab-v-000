0e84d59e1a112717a3f8e141477d49f5b51a8c4a
benhayehudi <bengreenberg@gmail.com>
2017-08-14 10:07:43 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsDiv = directors.map((director, index) => (
+    <div key={index}>
+      <p>{director.name}</p>
+      <ul>
+        {director.movies.map((movie, index) => ( <li>{movie}</li> ))}
+      </ul>
+    </div>
+  ));
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsDiv}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const moviesDiv = movies.map((movie, index) => (
+    <div key={index}>
+      <h1>Movie: {movie.title}</h1>
+      <p>Time: {movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {movie.genres.map((genre, index) => ( <li key={index}>{genre}</li>))}
+      </ul>
+    </div>
+  ));
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesDiv}
     </div>
   );
 };
