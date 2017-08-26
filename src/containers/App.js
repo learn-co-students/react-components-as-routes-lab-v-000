c0b61dc59b8bb046ee370933600c7a17e56bf1c0
lucketta <lucketta@umich.edu>
2017-08-25 21:33:18 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
+import {
+  BrowserRouter as Router,
   Route
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
@@ -13,7 +13,14 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <NavBar>
+        <div>
+        <Route exact path="/" render={Home} />
+        <Route exact path="/movies" render={Movies} />
+        <Route exact path="/directors" render={Directors} />
+        <Route exact path="/actors" render={Actors} />
+       </div>
+      </NavBar>
     </Router>
   );
 };
