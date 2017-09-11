20cd54ed5b9b66812f9e19808f888652d734f168
Jacob <jacob@Jacobs-Air.home>
2017-09-11 15:49:40 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,13 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Actors Page </h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          {actor.name}
+          {actor.movies}
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+          <div key={index}>
+              {movie.title}
+              {movie.time}
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
