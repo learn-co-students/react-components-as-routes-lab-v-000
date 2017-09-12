70041fc554d750d57b03fc6a5be5a579b5c4e8e3
Howard DeVennish <howarddevennish@gmail.com>
2017-09-11 21:52:30 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { directors } from '../data';
 
+let directorsHTML = directors.map(director=>{
+  return (<div>
+    <h1>{director.name}</h1>
+    <ul>
+      {director.movies.map(movie=><li>{movie}</li>)}
+    </ul>
+  </div>
+  );
+})
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      { directorsHTML }
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { movies } from '../data';
 
+let moviesHTML = movies.map(movie=>{
+  return (<div>
+    <h3>{movie.title}</h3>
+    <h5>{movie.time} minutes</h5>
+    <ul>
+      {movie.genres.map(genre=><li>{genre}</li>)}
+    </ul>
+    <h5>Metascore: {movie.metascore}</h5>
+  </div>
+  );
+})
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        { moviesHTML }
     </div>
   );
 };
