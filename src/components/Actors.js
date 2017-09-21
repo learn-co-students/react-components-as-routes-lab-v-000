d6c377417f10f87d112017427d493c9594626df7
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-21 12:59:03 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  
+  const actorLi = actors.map((actor, i) => {
+    const movieLi = actor.movies.map(movie => <li>{movie}</li>)
+    return (
+      <div>
+        <li key={i}>{actor.name}</li>
+        Movies
+        <ul>
+          {movieLi}
+        </ul>
+      </div>  
+    )
+  })
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      <ul>
+        {actorLi}
+      </ul>
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { movies } from '../data';
 
+// {
+//     title: 'Doctor Strange',
+//     time: 115,
+//     genres: ['Action', 'Adventure', 'Fantasy'],
+//     metascore: 74
+//   }
+
 const Movies = () => {
+
+  const movieLi = movies.map((movie, i) => {
+
+    const genreLi = movie.genres.map((genre, idx) => <li key={idx}>{genre}</li>)
+
+    return (
+      <div>        
+        Title: {movie.title} <br/>
+        Run Time: {movie.time} <br/>
+        Metascore: {movie.metascore} <br/>         
+        <ul>
+          {genreLi}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movieLi}
     </div>
   );
 };
