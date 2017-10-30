d8d4b75348e89b4d3f6a629f8df3a47f56a734c4
curlywallst <nnoyes@comcast.net>
2017-10-30 18:14:18 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { movies } from '../data';
 
+const getChildren = (genres) => {
+  return genres.map((genre) =>
+    <ul>{genre}</ul>)
+}
+
+const moviesList = movies.map((movie, index) => (
+    <div>{movie.title}{movie.time}{getChildren(movie.genres)}</div>
+  ))
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesList}
     </div>
   );
 };
