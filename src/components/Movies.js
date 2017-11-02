ad33350748e45c9df44cf31e915624aeb6a8c166
dustin <dandersonsoftware@outlook.com>
2017-11-02 00:20:10 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie) =>
+          <div>
+            <h1>{movie.title}</h1>
+
+            <ul>
+              <li>Runtime: {movie.time}</li>
+              <li>Genres: {movie.genres.join(', ')}</li>
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
