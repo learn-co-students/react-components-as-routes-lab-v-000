7214446cf53a2912a3290b060560fb1734961011
Sara Hopper <github email address>
2017-11-07 14:09:25 -0500

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+const link = {
+    color: 'blue',
+}
+
+const NavBar = () =>
+  <div className="navbar">
+    <ul>
+      <li>
+        <NavLink
+          to="/"
+          exact
+          style={link}
+        >Home</NavLink></li>
+      <li><NavLink
+        to="/movies"
+        exact
+        style={link}
+      >Movies</NavLink></li>
+      <li><NavLink
+        to="/directors"
+        exact
+        style={link}
+      >Directors</NavLink></li>
+      <li><NavLink
+        to="/actors"
+        exact
+        style={link}
+      >Actors</NavLink></li>
+    </ul>
+  </div>
 
 export default NavBar;
