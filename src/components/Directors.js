79008612126436bfcbc9a9023fed55c7b63c533b
Moshe Levi <mo6709@gmail.com>
2017-11-16 15:44:00 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsList = directors.map((director, index)=>{
+    return(
+      <div key={index} className="director">
+        <h2>{director.name}</h2>
+        <ul>
+          {director.movies.map((movie, i) => <li key={i}>{movie}</li>)}
+        </ul>  
+      </div>
+    );
+  });
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsList}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+const link = {
+  width: '',
+  padding: ''
+}
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+       to="/"
+       exact
+       style={link}
+      >Home</NavLink>
+
+      <NavLink
+       to="/movies"
+       exact
+       style={link}
+      >Movies</NavLink>
+
+      <NavLink
+       to="/directors"
+       exact
+       style={link}
+      >Directors</NavLink>
+
+      <NavLink
+       to="/actors"
+       exact
+       style={link}
+      >Actors</NavLink>
     </div>
   );
 };
