4330ce41f2a32d2f5a4460208ea81ba8cf1bc5fa
Cecily2 <cecily.downs@gmail.com>
2017-11-23 15:00:59 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { actors } from '../data';
 
+console.log(actors)
+
 const Actors = () => {
+  const actorList = actors.map((actor, key) => {
+    return <div className="actor" key={key}>{actor.name}
+      <ul>
+        {actor.movies.map((movie, i) => <li key="{i}">{movie}</li>)}
+      </ul>
+    </div>
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorList}
+
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { movies } from '../data';
 
+console.log(movies)
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, key) => {
+          return <div>
+              {movie.title}
+              <br />
+              Time: {movie.time}
+              <br />
+              Genres: {movie.genres.map((genre, key) => <span key={key}>{genre} </span>) }
+          </div>
+        })}
     </div>
   );
 };
