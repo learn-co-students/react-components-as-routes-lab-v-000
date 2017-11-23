c556286a0d06f9b02b4407eb8f26d1f6d8471090
Bryan Hou <bryan.hou94@gmail.com>
2017-11-22 23:44:49 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	const actorsDisplay = actors.map((actor) => {
+		const moviesDisplay = actor.movies.map((movie) => <li>{movie}</li>);
+		return (
+			<div className="actor">
+				{actor.name}
+				<ul>
+					{moviesDisplay}
+				</ul>
+			</div>
+		)
+	})
+
+	return (
+		<div>
+			<h1>Actors Page</h1>
+			{actorsDisplay}
+		</div>
+	)
 };
 
 export default Actors;
