c76f99424526ef9fca68c7c54d756d7c75cd3a61
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-12-02 15:51:39 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie, index) => {
+          return <div>
+            <h3>{movie['title']}</h3>
+            <h4>{movie['time']}</h4>
+            <ul>{movie['genres'].map((genre, index) => <li key={index}>{genre}</li>)}</ul>
+          </div>
+        })}
     </div>
   );
 };
