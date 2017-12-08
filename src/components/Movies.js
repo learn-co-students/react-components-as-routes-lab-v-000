7c28334edef73ba2a7f611a925e095c427b377c9
laramontana <larabrightside@gmail.com>
2017-12-09 00:47:09 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
+    return (
+      <div>
+          <h1>Movies Page</h1>
+            {movies.map(movie => {
+              const genres = movie.genres.map(genre => <li>{genre}</li>)
+              return <div>
+                <h3>Name: {movie.title}</h3>
+                <p>Time: {movie.time}</p>
+                <ul>
+                  {genres}
+                </ul>
+              </div>
+            })}
+      </div>
+    );
 };
 
 export default Movies;
+// This component should render the text Movies Page,
+// and make a new <div> for each movie.
+// The <div> should contain the movie's title, time and an <ul> for each genre.
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'GreenYellow',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'Green'
+      }}
+      >Home</NavLink>
+      <NavLink
+      to="/movies"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'Green'
+      }}
+      >Movies</NavLink>
+      <NavLink
+      to="/directors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'Green'
+      }}
+      >Directors</NavLink>
+      <NavLink
+      to="/actors"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'Green'
+      }}
+      >Actors</NavLink>
     </div>
   );
 };
