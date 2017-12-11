6766b343b7af442bbfe595683be3517889ad00b3
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-12-11 14:54:17 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const mappedActors = actors.map(actor => {
+    const movies = actor.movies.map(movie => <li>{ movie }</li>);
+    return (
+      <div>
+        <h1>{ actor.name }</h1>
+        <ul>{ movies }</ul>
+      </div>
+    );
+  });
   return (
+
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      { mappedActors }
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const mappedDirectors = directors.map(director => {
+    const movies = director.movies.map(movie => <li>{ movie }</li>);
+    return (
+          <div>
+            <h1>{director.name}</h1>
+            <ul>{movies}</ul>
+          </div>
+    );  
+  });
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {mappedDirectors}
     </div>
   );
 }
