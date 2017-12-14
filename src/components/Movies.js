ecd5ece6a72d72ce2b74eaf869813a0434abf72a
sukey <sdklos@gmail.com>
2017-12-13 21:39:45 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { movies } from '../data';
 
+const Movie = ( {movie: {title, time, genres}}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{time} minutes</p>
+      {genres.map(genre => <ul key={genre}>{genre}</ul>)}
+    </div>
+)}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map(movie =><div className="movie"><Movie key={movie.name} movie={movie} /></div>)}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 render (
   <App />,
   document.getElementById('root')
