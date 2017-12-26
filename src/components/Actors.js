1b5e2e22b21b7561845620ef827dd6f75afc72df
github username <github email address>
2017-12-26 09:42:03 -0600

    movies page

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  //console.log(actors)
+
+  var actorsDiv = actors.map(actor => {
+    var movies = actor.movies.map(movie => { return <li>{movie}</li>})
+    return (<div>{actor.name}<ul>{movies}</ul></div>)
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsDiv}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  var directorsDiv = directors.map(actor => {
+    var movies = actor.movies.map(movie => { return <li>{movie}</li>})
+    return (<div>{actor.name}<ul>{movies}</ul></div>)
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsDiv}
     </div>
   );
-}
+};
 
 export default Directors
