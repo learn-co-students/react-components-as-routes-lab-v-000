27ddb5362337e5ee9573ad273732cb5b34d419c1
Ynebuhs <shhubeny@gmail.com>
2017-12-26 21:26:15 -0700

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,12 +10,20 @@ import Directors from '../components/Directors';
 import Movies from '../components/Movies';
 
 
-const App = (props) => {
-  return (
-    <Router>
-      {/*{code here}*/}
-    </Router>
-  );
-};
+
+const App = () => {
+    return(
+      <Router>
+        <div>
+          <NavBar />
+          <Route exact path ="/" component={Home}/>
+          <Route exact path ="/movies" component={Movies}/>
+          <Route exact path ="/directors" component={Directors}/>
+          <Route exact path ="/actors" component={Actors}/>
+        </div>
+      </Router>
+    )
+}
+
 
 export default App
