8db5d8c7a4faec1c11f395160c90068266c34970
johanesteves <jsesteves91@gmail.com>
2018-01-02 09:58:14 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie)=> {
+          return (<div><ul>
+            <li>{movie.title} </li>
+            <li>{movie.time}  </li>
+            <ul>{movie.genres.map((genre) => {
+              return <li>{genre}</li>
+            })}</ul>
+            <li>{movie.metascore}</li>
+          </ul></div>)
+        })}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <ul>
+        <li><NavLink to="/" exact activeStyle={{background: 'darkblue'}}>Home</NavLink></li>
+        <li><NavLink to="/movies" exact activeStyle={{background: 'darkblue'}}>Movies</NavLink></li>
+        <li><NavLink to="/directors" exact activeStyle={{background: 'darkblue'}}>Directors</NavLink></li>
+        <li><NavLink to="/actors" exact activeStyle={{background: 'darkblue'}}>Actors</NavLink></li>
+      </ul>
     </div>
   );
 };
