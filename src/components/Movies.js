377635857f3bb8994ac7f25a9d6ac4fe554121df
Jim Stricker <jmstricker93@gmail.com>
2018-01-15 10:44:46 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movies.map((movie, index)=>{
+          return (
+            <div>
+              <h3>{movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <ul>
+                {movie.genres.map((genre, index) => {
+                  return(
+                    <li>{genre}</li>
+                  )
+                })}
+              </ul>
+            </div>)
+        })}
     </div>
   );
 };
