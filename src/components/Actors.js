fc8f59e1c1063aa0bb4c88a44bd8db181b081fd9
baschenbrenner <baschenbrenner@hotmail.com>
2018-02-07 16:26:05 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,11 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((el, index) => (<div key={index}><h2>Name: {el.name}</h2>
+        {actors[index].movies.map(movie=>(<li>{movie}</li>))}
+        </div>))}
+
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie,index)=>(<div key={index}><h2>Title: {movie.title}</h2>
+          <p>Running Time (minutes): {movie.time} </p><ul>
+          {movie.genres.map(genre => (<li>{genre}</li>))}
+          </ul>
+          </div>
+
+        ))}
     </div>
   );
 };
