66566830e0c9dbf7fc0772bdab3720052d442a9c
Joshua Palacios <palacios.joshua@gmail.com>
2018-02-07 21:37:19 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return <div>
+          Title: {movie.title}
+          Time: {movie.time}
+          Genre: {movie.genres.join(", ")}
+        </div>
+      })}
     </div>
   );
 };
