2b8564b7fe1400f9a0457806ba76bad19a9139d9
github username <github email address>
2018-02-07 16:13:20 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  const dirs = directors.map((dir, index) => (
+    <div key={index}>
+      <h3>Name: {dir.name}</h3>
+      <p>Movies:</p>
+      <ul>
+        {dir.movies.map((movie, i) => (<li key={i}>{movie}</li>))}
+      </ul>
+    </div>
+  ))
+
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="directors">
+      <h1>Directors Page</h1>
+      {dirs}
     </div>
   );
 }
