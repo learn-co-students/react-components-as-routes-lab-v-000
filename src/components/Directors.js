dcec3dee83f360e8878e4c5c5816260a5a9f0115
kyleblee <hello.kylelee@gmail.com>
2018-02-07 23:12:02 -0500

    lab complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsHTML = directors.map(director =>
+    <div key={director.name}>
+      <h4>{director.name}</h4>
+      <ul>
+        {director.movies.map(movie =>
+          <li key={movie}>{movie}</li>
+        )}
+      </ul>
+    </div>
+  )
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsHTML}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const moviesHTML = movies.map(movie =>
+    <div key={movie.title}>
+      <h4>{movie.title}</h4>
+      <p>Time: {movie.time}</p>
+      <p>Genres</p>
+      <ul>
+        {movie.genres.map(genre =>
+          <li key={genre}>{genre}</li>
+        )}
+      </ul>
+    </div>
+  );
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {moviesHTML}
     </div>
   );
 };
