46581b0696a2a643d8f317611165984c95c20d4f
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-13 22:26:29 -0800

    navbar tests pass, all tests pass

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,7 @@ const App = (props) => {
   return (
     <Router>
       <div>
+        <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/movies" component={Movies} />
         <Route exact path="/directors" component={Directors} />
