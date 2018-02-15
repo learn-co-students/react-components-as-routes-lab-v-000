ecca8b8fa12e95d8d182a22948b8d8f79847df18
Ccordova41 <ccordova41@gmail.com>
2018-02-14 20:00:04 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Title: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+          <p>Metascore:{movie.metascore}</p>
+        </div>
+      ))}
+
+
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -12,10 +12,17 @@ import Movies from '../components/Movies';
 
 const App = (props) => {
   return (
-    <Router>
-      {/*{code here}*/}
-    </Router>
-  );
+  <Router>
+    <div className="app">
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/actors" component={Actors} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/movies" component={Movies} />
+    </div>
+  </Router>
+);
 };
+
 
 export default App
