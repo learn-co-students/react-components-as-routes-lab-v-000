cdaf3ccb68d469117dfce2a6be075e9d7fc7029b
jessmitch42 <jessicamitch42@gmail.com>
2018-03-18 14:17:08 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,8 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(a => <div className="actor">{a.name}: {a.movies.join(", ")}</div>)}
     </div>
   );
 };
