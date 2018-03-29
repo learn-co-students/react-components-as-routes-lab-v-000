4de7036e4592b900a61b4b5670f8a91a7156da1a
CodeDad <github email address>
2018-03-29 14:46:41 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+       <h1>Directors Page </h1>
+      {directors.map((director) => (
+        <div>
+          <p>Name: {director.name}</p>
+          <p>Movies:</p>
+            <ul>
+              {director.movies.map((movie) => (
+                <li>{movie}</li>
+              ))} 
+              </ul>
+        </div> 
+      ))}
     </div>
   );
 }
 
 export default Directors
+
