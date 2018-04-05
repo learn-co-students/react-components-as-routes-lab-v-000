ba32391c0d3aa7b4f7c01b16f182f51e96201026
PJ Frias <PeejFrias@Gmail.com>
2018-04-05 09:54:22 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorsList = actors.map(actor => {
+    const movies = actor.movies.map(movie => {
+      return <li>{movie}</li>
+    })
+    return(
+      <div>
+        <strong>{actor.name}</strong>
+        <ul>
+          {movies}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsList}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,25 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const movieList = movies.map(movie => {
+    const genres = movie.genres.map(genre => {
+      return <li>{genre}</li>
+    })
+    return (
+      <div>
+        <strong>{movie.title}</strong><br/>
+        <em>{movie.time}</em>
+        <ul>
+          {genres}
+        </ul>
+        <br /><br />
+      </div>
+    )
+  })
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movieList}
     </div>
   );
 };
