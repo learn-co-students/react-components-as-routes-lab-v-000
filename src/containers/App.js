22ea2106b08a150628d47b0aef3981928c1ab792
Hannah Carnes <hecarnes@gmail.com>
2018-04-06 16:06:36 -0400

    add routes

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,12 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      <Route exact path='/actors' component={Actors} />
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/actors' component={Actors} />
+        <Route exact path='/directors' component={Directors} />
+        <Route exact path='/movies' component={Movies} />
+      </div>
     </Router>
   );
 };
