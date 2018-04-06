6cd45d280d8241e1fdb064fbaf582fa9d62d1338
Hannah Carnes <hecarnes@gmail.com>
2018-04-06 17:04:03 -0400

    done

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route, NavLink
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
@@ -14,10 +14,11 @@ const App = (props) => {
   return (
     <Router>
       <div>
+        <NavBar />
         <Route exact path='/' component={Home} />
+        <Route exact path='/movies' component={Movies} />
         <Route exact path='/actors' component={Actors} />
         <Route exact path='/directors' component={Directors} />
-        <Route exact path='/movies' component={Movies} />
       </div>
     </Router>
   );
