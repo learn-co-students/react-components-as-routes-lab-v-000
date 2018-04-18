0344520851b065144592628858a34e899c6a10f3
Alexander Urizar <urizaraj@gmail.com>
2018-04-18 10:53:11 -0400

    complete lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorList = directors.map(({name, movies}) => {
+    const movieList = movies.map(movie => ( <li key={name} > {movie} </li> ))
+
+    return (
+    <div key={name} >
+      <h4>{name}</h4>
+      <ul>
+        {movieList}
+      </ul>
+    </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorList}
     </div>
-  );
+  )
 }
 
 export default Directors
