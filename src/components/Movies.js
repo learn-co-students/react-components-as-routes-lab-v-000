b1bb7b07858cbfd60e3e7dfe372add331d2e1119
jennianelson <jennifernelson118@gmail.com>
2018-05-14 14:00:37 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return (<div key={movie.title}>
+            <h3>{movie.title}</h3>
+            <p>{movie.time}</p>
+            <ul>
+              {movie.genres.map(genre => {
+                return (<li key={genre}>{genre}</li>)
+              })}
+            </ul>
+          </div>)
+        })}
     </div>
   );
 };
