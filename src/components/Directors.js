90a2f72709f8092955efd500f0cf086132a21050
ingridwong0715 <ingrid830715@gmail.com>
2018-05-20 18:41:22 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  let directorsDisplay = directors.map( director => {
+    return (
+      <div>
+        {director.name}
+        <ul>
+          {director.movies.map( movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsDisplay}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  let moviesDisplay = movies.map( movie => {
+    return (
+      <div>
+        {movie.title}
+        {movie.time}
+        <ul>
+          {movie.genres.map( genre => <li> {genre} </li>)}
+        </ul>
+      </div>
+    );
+  });
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {moviesDisplay}
     </div>
   );
 };
