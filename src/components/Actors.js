87d4db8d91fbcc5bbf9f5c2c5e9f82f04cce3506
github username <schun08@gmail.com>
2018-05-28 17:27:16 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import { actors } from '../data';
 
-const Actor = ({actor}) => {
-  return (
-    <div className="actor">
-      <p>Name: {actor.name}</p>
-      <p>Movies:</p>
-      <ul>
-        {actor.movies.map((movie, idx) => <li key={idx}>{movie}</li>)}
-      </ul>
-    </div>
-  )
-}
-
 const Actors = () => {
   return (
     <div>
