c290feebcdc95f4e178e5b6a10b293a4f2b8d3c8
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-08 21:07:13 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorsList = actors.map((actor, index) => {
+    return(<div key={index}>
+      <p>{actor.name}</p>
+      <ul>
+      {actor.movies.map((movie, index) => {
+        return <li key={index}>{movie}</li>
+      })}
+      </ul>
+      </div>
+    )
+  })
+
+
+
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {actorsList}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorsList = directors.map((director, index) => {
+    return (<div key={index}>
+      <p>{director.name}</p>
+      <ul>
+      {director.movies.map((movie, index) => {
+        return <li key={index}>{movie}</li>
+      })}
+      </ul>
+      </div>
+    )
+  })
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Directors Page </h1>
+      {directorsList}
     </div>
   );
 }
