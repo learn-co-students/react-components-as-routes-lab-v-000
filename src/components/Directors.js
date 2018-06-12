67937de2bd15e191e2b446fe46c6553bacfc4584
Adjoa <adjoa.darien@gmail.com>
2018-06-12 13:21:02 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,29 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const allDirectors = directors.map((director, index) => {
+   let directorMovies = director.movies.map((movie, ndx) => {
+     return (
+       <li key={ndx}>{movie}</li>
+       )
+   })
+  
+  return (
+    <div key={index}>
+      <h2>{director.name}</h2>
+      <ul>
+      {directorMovies}
+      </ul>
+    </div> 
+    ) 
+  })
+  
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {allDirectors}
     </div>
   );
 }
 
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+   const allMovies = movies.map((movie, index) => {
+    let movieGenres = movie.genres.map((genre, ndx) => {
+     return (
+       <li key={ndx}>{genre}</li>
+       )
+    })
+   
+    return (
+      <div key={index}>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <ul>
+        { movieGenres }
+        </ul>
+      </div> 
+    ) 
+  })
+  
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      { allMovies }
     </div>
   );
 };
