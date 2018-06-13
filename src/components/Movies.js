9be099f35170f5951e7d9a561660978ed902fd43
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-06-13 12:15:25 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, idx) => (
+        <div key={idx}>
+        <h1>{movie.title}</h1>
+        <p>Time: {movie.time}</p>
+        <h4>Genres</h4>
+        <ul>
+          {movie.genres.map((genre, idx) => (<li key={idx}>{genre}</li>))}
+        </ul>
+        </div>
+      ))}
     </div>
   );
 };
