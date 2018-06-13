935aeab5cfc090510c0100e61812132b345d980b
Hoon Lee <yl.hoony@gmail.com>
2018-06-12 23:51:47 -0700

    implemented

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorsList = directors.map((director, index) => {
+  return (
+    <div>
+      <h2 key={index}>{director.name}</h2>
+      <p>Movies: {director.movies.join(", ")}</p>
+    </div>
+  )
+})
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsList}
     </div>
   );
 }
