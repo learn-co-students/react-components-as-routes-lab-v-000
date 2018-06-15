c3929343b204fc045812f4f97c328104f20f919c
Chris Boujoukos <github email address>
2018-06-15 11:40:18 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { actors } from '../data';
 
-const Actors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+class Actors extends Component{
+  render(){
+    const actorList = actors.map((actor, i) =>
+      <div key={i}>
+        <h3>{actor.name}</h3>
+        Movies:
+        <ul>
+          {actor.movies.map(m => <li>{m}</li>)}
+        </ul>
+      </div>
+    )
+    return(
+      <div>
+        <h1>Actors Page</h1>
+        {actorList}
+      </div>
+    )
+  };
 };
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { directors } from '../data';
 
-const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-}
+class Directors extends Component{
+  render(){
+    const directorList = directors.map((director, i) =>
+      <div key={i}>
+        <h3>{director.name}</h3>
+        Movies:
+        <ul>
+          {director.movies.map(m => <li>{m}</li>)}
+        </ul>
+      </div>
+  )
+    return(
+      <div>
+        <h1>Directors Page</h1>
+        {directorList}
+      </div>
+    )
+  };
+};
 
-export default Directors
+export default Directors;
