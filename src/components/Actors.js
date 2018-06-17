7e2429c8e844e33144a1896060e8e4d3bb89d8b2
Jordan Kline <jordan.kline@gmail.com>
2018-06-17 16:05:29 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,10 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    	<h1>Actors Page</h1>
+    	{actors.map((actor, index) => {
+    		return <div key={index}>Actor: {actor.name}, Movies: {actor.movies.join(', ')}</div>
+    	})}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map((movie, index) => {
+        	return (<div key={index}>
+        				Title: {movie.title} <br />
+        				Duration: {movie.time} <br />
+        				Genres: {movie.genres.join(', ')} <br />
+        				Metascore: {movie.metascore}<br /></div>);
+        })}
     </div>
   );
 };
