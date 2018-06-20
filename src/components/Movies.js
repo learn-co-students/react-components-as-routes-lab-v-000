9c002ecedc90dba3c8c973b18631f7f3372a8321
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-20 10:06:51 -0400

    Movies Tests passing

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,43 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const movieList = movies.map((m) => {
+
+    // console.log("Actor Object: ", a)
+    const genres = m.genres.map((g) => {
+      return (<li> {g} </li>)
+    })
+
+    console.log("Movies:", movies)
+
+    return (
+      <div className="director">
+      {m.title} <br/>
+      {m.time}
+      Genres:
+        <ul>
+          {genres}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {movieList}
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <h1>Movies Page</h1>
+  //
+  //
+  //       {/*{code here}*/}
+  //   </div>
+  // );
 };
 
 export default Movies;
