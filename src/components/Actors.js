bc69d582d18a1df7b9f959fadf6bf9e0a19bb8e4
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-06-22 16:41:07 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,11 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    	<h1>Actors Page</h1>
+    	{actors.map((actor, index) => {
+    		return <div key={index}>Actor: {actor.name}, Movies: {actor.movies.join(', ')}</div>
+    	})}
     </div>
   );
 };
-
 export default Actors;
