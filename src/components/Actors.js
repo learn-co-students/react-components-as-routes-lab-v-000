19e371590d6cfcdee93888fc18bb165fd56c0136
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-07-03 17:48:36 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+
+  const actorList = actors.map((actor) => {
+    return <div className="actor">{actor.name}
+      
+      <ul>{actor.movies}</ul>
+    </div>
+  })
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorList}
     </div>
   );
 };
