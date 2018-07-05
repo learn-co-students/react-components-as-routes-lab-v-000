12c05d3137e8c922b843e4a3b13c975f362a272f
NolanHughes <Nhughes987@gmail.com>
2018-07-05 15:26:15 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,8 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => <div key={index + 1} className='movie'>{movie.title}{movie.time}{movie.genres.map((genre, genreIndex) => <p key={genreIndex + 1}>{genre}</p>)}</div>)}
     </div>
   );
 };
