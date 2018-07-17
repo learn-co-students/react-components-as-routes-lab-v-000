dda9cae883e28b305d6b4e5092100c08a7428020
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-07-17 14:11:45 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const displayActors = actors.map(actor => {
+    return (
+      <div className='actor'>
+        <h3>Name: {actor.name}</h3>
+        <p>Movies:</p>
+        <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {displayActors}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const displayMovies = movies.map(movie => {
+    return (
+      <div className='movies'>
+        <h3>Name: {movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </div>
+    )
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {displayMovies}
     </div>
   );
 };
