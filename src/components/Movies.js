15175fe18342535da4c554d16d307c656206089d
bretburau <github email address>
2018-07-22 18:02:53 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return(
+          <div>
+            Title: {movie.title}<br />
+            Runtime: {movie.time}<br />
+            <ul>
+              {movie.genres.map((genre) => {
+                return(
+                  <li>{genre}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
