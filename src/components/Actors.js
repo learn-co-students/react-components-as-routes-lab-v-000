865ba7cb8713f8b37bfcb1c6aaf3ac281d95090f
Maryna Voitenko <maryna.voitenko@gmail.com>
2018-07-27 21:18:36 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { actors } from '../data';
 
+const card = {
+  width: '400px',
+  padding: '5px 15px',
+  margin: '0 5px 5px',
+  border: '1px solid grey',
+}
+
 const Actors = () => {
+  const actorsDisplay = actors.map((actor, index) => 
+    <div key={index} className="card">
+      Name: {actor.name}
+      <br />
+      Movies: <ul>{actor.movies.map((movie, index) => <li key={index}>{movie}</li>)}</ul>
+    </div>
+  );
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        {actorsDisplay}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const navlink = {
+  width: '100px',
+  padding: '10px',
+  margin: '0 5px 5px',
+  background: 'grey',
+  textDecoration: 'none',
+  color: 'white',
+}
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink 
+        to= "/"
+        exact
+        style={navlink}
+      >Home</NavLink>
+      <NavLink 
+        to= "/movies"
+        exact
+        style={navlink}
+      >Movies</NavLink>
+      <NavLink 
+        to= "/directors"
+        exact
+        style={navlink}
+      >Directors</NavLink>
+      <NavLink 
+        to= "/actors"
+        exact
+        style={navlink}
+      >Actors</NavLink>
     </div>
   );
 };
