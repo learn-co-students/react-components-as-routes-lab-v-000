568cb105349dab15120e18c100b898d52d9d1224
Randall Robinson <keith_adams007@yahoo.com>
2018-08-05 23:55:06 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  for(const movie of movies){
   return (
     <div>
-        {/*{code here}*/}
+        {movie.title}
+        {movie.time}
+      for(const genre of {movie.genres}) {
+        <ul>genre</ul>
+      }
     </div>
   );
+}
 };
 
 export default Movies;
