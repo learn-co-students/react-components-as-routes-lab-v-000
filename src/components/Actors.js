f65d1097e2385b067c50dbc42607491e03853a69
ritchey0713 <ritchey0713@gmail.com>
2018-08-13 17:50:54 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1> Actors Page </h1>
+      {actors.map((actor, index) => (
+        <div>
+          <h4 className="actor:" key={index}> Name:{actor.name}</h4>
+          <ul>
+          {actor.movies.map((movie, index) => (
+            <li key={index}>
+              {movie}
+            </li>
+          ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,40 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div>
+          <h1 key={index}>Name: {movie.title}</h1>
+          <h4>Time: {movie.time}</h4>
+          {movie.genres.map((genre, index) =>(
+            <ul>
+            {genre}
+            </ul>
+          ))}
+        </div>
+      ))}
+
     </div>
-  );
-};
+  )
+}
 
 export default Movies;
+
+// let movies = [
+//   {
+//     title: 'Doctor Strange',
+//     time: 115,
+//     genres: ['Action', 'Adventure', 'Fantasy'],
+//     metascore: 74
+//   },
+//   {
+//     title: 'Trolls',
+//     time: 92,
+//     genres: ['Animation', 'Adventure', 'Comedy', 'Family', 'Fantasy']
+//   },
+//   {
+//     title: 'Jack Reacher: Never Go Back',
+//     time: 118,
+//     genres: ['Action', 'Adventure', 'Crime', 'Mystery', 'Thriller']
+//   }
+// ]
