e44050610411a640aa01a80ee14ca14d53d785db
kostanzhoglo <jeffreywithers@gmail.com>
2018-08-16 09:43:06 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,18 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => (
+        <div key={idx}>
+          <h2>{actor.name}</h2>
+          <h4>Movies:</h4>
+            <ul>
+              {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,18 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((dir, idx) => (
+        <div key={idx}>
+          <h2>{dir.name}</h2>
+          <h4>Movies:</h4>
+            <ul>
+              {dir.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+        </div>
+      ))}
     </div>
   );
 }
