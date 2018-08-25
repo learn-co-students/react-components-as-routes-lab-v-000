291a48b09473b59a79cb2bd1ea8c6192aad74172
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-08-25 13:33:01 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { directors } from '../data';
 
+// Khaled broke each director component into even smaller components so each can be handled separately
+
+const MovieContainer = ({ children }) => (
+  <ul>{ children }</ul>
+);
+
+const Movie = ({ title }) => (
+  <li>{ title }</li>
+);
+
+const Director = ({name, movies}) => (
+  <div>
+    { name }
+    <h4>Movies</h4>
+    <MovieContainer>
+      { movies.map(movie => <Movie title={ movie }/>) }
+    </MovieContainer>
+  </div>
+);
+
 const Directors = () => {
   return (
     <div>
       <h1> Directors Page </h1>
       {
         directors.map(director => {
-          return (
-            <div>
-              { director.name }
-              <h4>Movies</h4>
-              <ul>{
-                director.movies.map(movie => <li>{ movie }</li>)
-              }</ul>
-            </div>
-          );
+          return <Director name={ director.name } movies={ director.movies } />
         })
       }
     </div>
