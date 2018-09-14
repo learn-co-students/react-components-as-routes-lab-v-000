06db61547d40e90082974fbedd19b09542f0176f
Kiveo <mavericktesini@gmail.com>
2018-09-13 17:00:32 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => 
+        <div key={index} className="actor">
+          {actor.name}
+          <ul style={{listStyleType: "none"}}>
+            {actor.movies.map((movie, index) => 
+            <li key={index}>
+              {movie}
+            </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
