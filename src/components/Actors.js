4df9548272eefa2b552ee90f674c6d7618475ecc
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-09-13 22:43:42 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { actors } from '../data';
 
+const rednerActors = (actors) => {
+  return actors.map((actor, idx) => {
+    return (
+      <div key={idx}>
+        <h2>{actor.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {actor.movies.map((m, i) => <li key={i}>{m}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {rednerActors(actors)}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { directors } from '../data';
 
+const renderDirectors = (directors) => {
+  return directors.map((director, idx) => {
+    return (
+      <div key={idx}>
+        <h2>{director.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {director.movies.map((m, i) => <li key={i}>{m}</li>)}
+        </ul>
+      </div>
+    )
+  })
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors(directors)}
     </div>
   );
 }
