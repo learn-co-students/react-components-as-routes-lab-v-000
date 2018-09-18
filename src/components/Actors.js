9f99ef2bd629b38f76b4a9bae1b4898bf6a636b0
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-09-18 11:03:15 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div>
+          <h1>{actor.name}</h1>
+          <ul>
+            {actor.movies.map(movie => (
+              <li>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div>
+          <h1>{movie.title}</h1>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
