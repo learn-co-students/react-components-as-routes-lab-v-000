68dace4a61ed490a291cdbfe82c6ab46e76d84ea
nptravis <nptravis@yahoo.com>
2018-09-19 17:20:14 +0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,16 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+        {directors.map(movie => 
+        	<div key={movie}>
+        	<h3>{movie.name}</h3>
+   
+        	<ul>
+        	{movie.movies.map(genre => <li>{genre}</li>)}
+        	</ul>
+        	</div>
+        	)}
     </div>
   );
 }
