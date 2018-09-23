4c018d75d9f0d553ef3ee261c5b76695542e72c7
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-09-23 00:24:12 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+        {movies.map((x, i) => {
+          return(
+            <div key={i}><br></br>
+              <h3> Title: {x.title} </h3>
+              <h4> Time: {x.time} </h4>
+              <h4> Genres: {x.genres} </h4>
+            </div>
+          )
+        })}
     </div>
   );
 };
