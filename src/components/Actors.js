b875723ee3ad1ec54197daa8cdf0a6e78c31ea86
github username <purnima.nyc@gmail.com>
2018-09-24 22:36:37 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      {
+      <div>
+      <h1>Actors Page</h1>
+        {actors.map(actor =>
+          <div>
+            <h2>{actor.name}</h2>
+            <p>{actor.movies}</p>
+          </div>
+        )}
+        </div>
+      }
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,14 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div>
+            <h2>{movie.title}</h2>
+            <p>{movie.time}</p>
+            <p>{movie.genres.join(', ')}</p>
+          </div>
+        )}
     </div>
   );
 };
