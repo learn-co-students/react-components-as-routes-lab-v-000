a427931a143df3ebe303e4c6d032346d8c265af6
Deborah Seow <deborahseow9@gmail.com>
2018-09-28 15:04:06 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+      {movies.map(movie =>
+      <div>
+        Title: {movie.title}
+        <br/>
+        Duration: {movie.time}
+        <br/>
+        Genres: {movie.genres.map(genre => <ul>{genre}</ul>)}
+      </div>
+    )}
     </div>
   );
 };
