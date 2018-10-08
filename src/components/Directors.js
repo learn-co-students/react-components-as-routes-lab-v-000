8bdd9cc4f47f135ecf66cbe0391de7680cc3f575
nmlstyl <joey.michalina.mariano@gmail.com>
2018-10-08 19:08:44 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -6,8 +6,23 @@ const Directors = () => {
 
     <div>
       <h1>Directors Page</h1>
+      { generateDirectors() }
     </div>
   );
+}
+
+function generateDirectors(){
+  return directors.map((director, index) =>
+      <div key={index}>
+          <h3>Name: {director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+              {director.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+              ))}
+          </ul>
+      </div>
+  )
 }
 
 export default Directors
