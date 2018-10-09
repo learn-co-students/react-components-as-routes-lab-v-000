40449e13d9b3719e41bc96fc9e04bb71c22965eb
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-10-09 17:19:18 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,7 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <h1>Directors Page</h1>{directors.map(x => <div><h3>{x.name}</h3><ul>{x.movies.map(y => <li key={y}>{y}</li>)}</ul></div>)}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,7 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>{movies.map(x => <div><h3>{x.title}</h3><p>{x.time}</p><ul>{x.genres.map(y => <li key={y}>{y}</li>)}</ul></div>)}
     </div>
   );
 };
