3233bd0a7b7c2b75749438999104bdc9678940c5
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-10-10 21:32:28 -0400

    Implement and style NavBar. Define routes. Implement Movies component/route

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+
 const Movies = () => {
+  function generateGenreList(genres){
+    return (
+      <ul>
+        {genres.map((genre) => { return <li>{genre}</li>})}
+      </ul>
+    )
+  }
+  function generateMovieList() {
+    return movies.map((movie) => {
+      return (
+        <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.time} minutes</p>
+          {generateGenreList(movie.genres)}
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {generateMovieList()}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  fontFamily: 'sans-serif',
+  background: '#DDD',
+  color: 'white',
+  textDecoration: 'none',
+  width: '80px',
+  padding: '10px',
+  margin: '0 5px 5px',
+}
+
+const activeLink = {
+  background: 'orange'
+}
+
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink exact to="/" style={link} activeStyle={activeLink}>Home</NavLink>
+      <NavLink exact to="/movies" style={link} activeStyle={activeLink}>Movies</NavLink>
+      <NavLink exact to="/actors" style={link} activeStyle={activeLink}>Actors</NavLink>
+      <NavLink exact to="/directors" style={link} activeStyle={activeLink}>Directors</NavLink>
     </div>
   );
 };
