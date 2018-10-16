5830cbbf1cc02f2a8b2cdfbf84eba767dc5726ba
TylerP33 <tyler3321@gmail.com>
2018-10-16 15:01:58 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,19 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, idx) => (
+          <div key={idx}>
+            <h2>{movie.title}</h2>
+            <h4>Time: {movie.time}</h4>
+            <h4>Genres:</h4>
+              <ul>
+                {movie.genres.map((genre, index) => (
+                  <li key={index}>{genre}</li>
+                ))}
+              </ul>
+          </div>
+        ))}
     </div>
   );
 };
