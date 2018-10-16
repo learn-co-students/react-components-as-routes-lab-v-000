a254cdb9520c7c6b2c07b36192e1e73bbb9bb720
Courtney <chalt0319@gmail.com>
2018-10-16 14:47:19 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+const data = () => {
+  return actors.map(function (actor) {
+    return <div key={actor.name} className="actor"><h3>{actor.name}</h3><p>Movies</p><ul>{movies(actor.movies)}</ul></div>
+  })
+}
+
+const movies = (array) => {
+  return array.map(function (movie) {
+    return <li key={movie}>{movie}</li>
+  })
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page!</h1>
+      {data()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { movies } from '../data';
 
+const data = () => {
+  return movies.map(function (movie) {
+    return <div key={movie.title}><h3>{movie.title}</h3><p>Time: {movie.time}</p><p>Genres</p><ul>{genres(movie.genres)}</ul></div>
+  })
+}
+
+const genres = (array) => {
+  return array.map(function (genre) {
+    return <li key={genre}>{genre}</li>
+  })
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page!</h1>
+        {data()}
     </div>
   );
 };
