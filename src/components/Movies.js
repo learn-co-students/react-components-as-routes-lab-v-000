91c7d3fe8ea9acdd9cd490deb90ce0076602f222
BrownCow371 <jmbrowncow@gmail.com>
2018-10-22 10:51:23 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
 
+const BuildMovieList = () => {
+  return (movies.map((movie, key)=> (
+    <div className="movie" key={key}>
+      <h2>Name: {movie.title}</h2>
+      <h3>Time: {movie.time}</h3>
+      <h3>Genres:</h3>
+      <ul><BuildGenreList genres={movie.genres}/></ul>
+    </div>
+      )))
+}
+
+const BuildGenreList = ({genres})=>{
+  return (genres.map((genre, key) => <li key={key}>{genre}</li>))
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        <BuildMovieList />
     </div>
   );
 };
