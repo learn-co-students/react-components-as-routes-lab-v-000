0a525d234df2fc203b1a0b865533942c955245ec
github username <github email address>
2018-10-22 20:46:35 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,17 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor,idx)=> (
+        <div key={idx} className='actor'>
+        <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie, idx)=>(
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,17 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director,idx)=> (
+        <div key={idx} className='director'>
+        <h3>{director.name}</h3>
+          <ul>
+            {director.movies.map((movie, idx)=>(
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
