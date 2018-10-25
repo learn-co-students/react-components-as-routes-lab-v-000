cec28bca8badd3a24c9cef08b55c4be2e56d530e
Emi Ko <emi8emi@gmail.com>
2018-10-25 16:50:33 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
-  return (
+  return(
+
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+
+      {movies.map(movie =>
+      <div>
+      <h4>{movie.title}</h4>
+      <p>{movie.time}</p>
+      <ul>{movie.genres.map((genre,index) =>
+        <li key={index}>{genre}</li>)}</ul>
+          </div>
+      )}
+
     </div>
-  );
-};
+
+
+)};
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,31 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <li><NavLink
+        to="/"
+        activeStyle={{
+          color: 'orange'
+      }}
+      >Home</NavLink></li>
+      <li><NavLink
+        to="/movies"
+        activeStyle={{
+          color: 'orange'
+      }}
+      >Movies</NavLink></li>
+      <li><NavLink
+        to="/directors"
+        activeStyle={{
+          color: 'orange'
+      }}
+      >Directors</NavLink></li>
+      <li><NavLink
+        to="/actors"
+        activeStyle={{
+          color: 'orange'
+      }}
+      >Actors</NavLink></li>
     </div>
   );
 };
