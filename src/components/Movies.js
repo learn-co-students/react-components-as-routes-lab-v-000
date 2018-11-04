11192469d5993becd027ace1da581eb437a7edeb
nlisauskas <nlisausk@alumni.nd.edu>
2018-11-04 11:45:20 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+         <div key={index}>
+             <h3>Title: {movie.title}</h3>
+             <h4>Time: {movie.time}</h4>
+             <p>Genres:</p>
+             <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+             </ul>
+             <h4>Score: {movie.metascore}</h4>
+         </div>
+     ))}
     </div>
   );
 };
