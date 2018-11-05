000eb42c4899a055064ccaad62871db1a99164c0
peterckim <peterckim96@gmail.com>
2018-11-05 15:59:16 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink
+        to="/"
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        activeStyle={{
+          background: "darkblue"
+        }}
+      >
+        Actors
+      </NavLink>
     </div>
   );
 };
