d411766938be4206e17fee9aebf89422245c55a8
cjwood032 <cjwood032@gmail.com>
2018-11-18 12:21:27 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 render (
   <App />,
   document.getElementById('root')
