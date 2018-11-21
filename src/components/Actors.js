65316efa03227d2ba351bfe6a662fc234e48c3f0
tsyaeger <tsyaeger@gmail.com>
2018-11-20 17:37:16 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+		<div className='actors'>
+		<h1>Actors Page</h1>
+			{actors.map(actor =>
+				<React.Fragment>
+					<h3>{actor.name}</h3>
+					<ul>
+						{actor.movies.map(movie => 
+							<li>{movie}</li>
+						)}
+					</ul>
+				</React.Fragment>
+			)}
+		</div>
+	);
 };
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,11 +2,30 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
+	return (
+
+		<div>
+			<h1>Directors Page</h1>
+
+			{directors.map(director =>
+
+				<div>
+					<h3>{director.name}</h3>
+
+					<ul>
+						{director.movies.map(movie =>
+					
+								<li>{movie}</li>
+					
+						)}
+					</ul>
+
+				</div>
+
+			)}
+
+		</div>
+	)
 }
 
 export default Directors
