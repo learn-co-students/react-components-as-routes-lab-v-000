a697caaf175afd743fbc453b55ce7e27e4960661
tsyaeger <tsyaeger@gmail.com>
2018-11-20 17:39:07 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,18 +3,20 @@ import { actors } from '../data';
 
 const Actors = () => {
 	return (
-		<div className='actors'>
+		<div>
 		<h1>Actors Page</h1>
+
 			{actors.map(actor =>
-				<React.Fragment>
+				<div>
 					<h3>{actor.name}</h3>
 					<ul>
 						{actor.movies.map(movie => 
 							<li>{movie}</li>
 						)}
 					</ul>
-				</React.Fragment>
+				</div>
 			)}
+
 		</div>
 	);
 };
