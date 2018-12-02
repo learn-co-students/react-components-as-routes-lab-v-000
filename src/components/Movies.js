a22f1598e7dfe301e814204d9945bc5c9aec60c5
Jrodr4544 <maskdbandit@gmail.com>
2018-12-02 06:04:08 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const movieCards = movies.map((movie, index)  => <div key={index} >Title: {movie.title}, Time: {movie.time}, Genres: {movie.genres.map(genre => <ul>{genre}</ul>)} </div>)
+
   return (
     <div>
-        {/*{code here}*/}
+	<h1>Movies Page</h1>
+        {movieCards}
     </div>
   );
 };
 
 export default Movies;
+//title: 'Doctor Strange',
+//    time: 115,
+//    genres: ['Action', 'Adventure', 'Fantasy'],
+//    metascore: 74
