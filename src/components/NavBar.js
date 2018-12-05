3bffaec52b9c72561d9424ca90fac9a532ff5724
allisonkadel <owner@allisonkadel.com>
2018-12-05 12:20:25 -0800

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+    return (
+      <div className='navbar'>
+        <NavLink to='/' exact activeStyle={{background: 'pink'}}>
+          Home
+        </NavLink>
+        <NavLink to='/movies' exact activeStyle={{background: 'pink'}}>
+          Movies
+        </NavLink>
+        <NavLink to='/directors' exact activeStyle={{background: 'pink'}}>
+          Directors
+        </NavLink>
+        <NavLink to='/actors' exact activeStyle={{background: 'pink'}}>
+          Actors
+        </NavLink>
+      </div>
+    );
+  }
 
 export default NavBar;
