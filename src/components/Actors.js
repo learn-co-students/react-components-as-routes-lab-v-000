15351fb833640c8a9ce1b7654e843d3f04d9639f
Richard Steinbach <steinbach.dick@gmail.com>
2018-12-13 12:09:08 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((item, index) => (
+        <div key={index}>
+          <p>{item.name}</p>
+          <ul>
+            {item.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+
+        {movies.map((item, index) => (
+          <div key={index}>
+            <p>{item.title}</p>
+            <p>{item.time}</p>
+
+            <ul>
+              {item.genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
     </div>
+
   );
 };
 
