56c6249a014fd70db8fa180527c84e49460bbb53
github username <github email address>
2018-12-18 17:12:44 -0600

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
-};
+class Movies extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movies Page</h1>
+          {movies.map((movie) => {
+            return (
+            <div>
+              <h2>Name: {movie.title}</h2>
+              <h3>Time: {movie.time}</h3>
+              <h3>Genres:</h3>
+              <ul>
+                {movie.genres.map(genre => {
+                  return(<li>{genre}</li>)
+                })}
+              </ul>
+            </div>
+            )
+          })}
+      </div>
+    )
+  }
+}
 
 export default Movies;
