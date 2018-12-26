e7a1508f3b9315b978a0686acd439217268cc679
Freddymorato <Freddymorato@gmail.com>
2018-12-26 17:55:35 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,9 +3,17 @@ import { directors } from '../data';
 
 const Directors = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <React.Fragment>
+      <h1>Directors Page</h1>
+      {directors.map(director =>
+        <div>
+          <h3>{director.name}</h3>
+          <ul>
+            {director.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,9 +3,19 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
-    </div>
+    <React.Fragment>
+      <h1>Movies Page</h1>
+      {movies.map(movie =>
+        <div>
+          <h1>{movie.title}</h1>
+          <p>{movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+          <p>{movie.metascore}</p>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
