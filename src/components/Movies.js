6354cc8fb9abd37be3f321a1ac223a634111c9ce
AubreyNeimeier <neimeier.1@osu.edu>
2019-01-09 10:16:32 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,8 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1> Movies Page </h1>
+        {movies.map(x => <div className={x.title}> <h1> Name: {x.title} </h1> Time: {x.time} Genres: <ul>  {x.genres.map(y => <li> {y} </li>)} </ul></div>)}
     </div>
   );
 };
