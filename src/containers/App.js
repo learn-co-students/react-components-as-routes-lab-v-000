bcdbcd44b605fe0993db61a5de3445254d28305d
edb-c <edb.cofresi@gmail.com>
2019-01-10 18:59:02 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -13,7 +14,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div className="app">
+        <NavBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/movies' component={Movies} />
+        <Route exact path='/directors' component={Directors} />
+        <Route exact path='/actors' component={Actors} />
+      </div>
     </Router>
   );
 };
