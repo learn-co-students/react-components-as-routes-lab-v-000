d50cdc09eda5ac03bc3d1d71f7ebaabc08b099bf
pcherian10 <pcherian10@gmail.com>
2019-01-15 14:04:45 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,9 +3,30 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
+
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map( movie =>
+
+            <div>
+
+            <h3> { movie.title } <h5> { movie.time } minutes </h5> </h3>
+
+            <ul>
+              <h3> Genres </h3>
+              {movie.genres.map( genre =>
+                  (<li> { genre }</li>)
+                ) }
+            </ul>
+
+            <h3> { movie.metascore } </h3>
+
+          </div>
+
+        )
+      }
     </div>
+
   );
 };
 
