f2b8dd6c19e80ef6fd20ac9f3c7a5498f2652ce6
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-02-17 23:36:55 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,10 +3,21 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='testactor'>
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.name} className='actor'>
+          <h3>{actor.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+          </ul>
+          <hr></hr>
+        </div>
+      ))}
     </div>
   );
 };
+
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title} className='movie'>
+          <h3>{movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+          {movie.genres.map(genre => <li key={genre}>{genre}</li> )}
+          </ul>
+          <hr></hr>
+        </div>
+      ))}
     </div>
-  );
-};
+  )
+}
+
 
 export default Movies;
