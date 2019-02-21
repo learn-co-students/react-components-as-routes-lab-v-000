67cf3d3743fb27e6917c59d91fd2944cd1f052bb
abourke09 <aabourke@gmail.com>
2019-02-21 14:50:37 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,15 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+
+      {actors.map((actor, index) =>
+        <div key={actor.name}>
+          <br></br>
+          <h3>Name: {actor.name}</h3>
+          <p>Movies: </p><ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+        </div>
+        ) }
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+
+      {movies.map((movie, index) =>
+        <div key={movie.title}>
+          <br></br>
+          <h3>Title: {movie.title}</h3>
+          <p>Time: {movie.time} minutes</p>
+          <p>Genres: </p><ul>{movie.genres.map(genre => <li key={genre}>{genre}</li>)}</ul>
+          {movie.metascore ? <p>Metascore: {movie.metascore}</p> : <p></p>}
+        </div>
+        ) }
     </div>
   );
 };
