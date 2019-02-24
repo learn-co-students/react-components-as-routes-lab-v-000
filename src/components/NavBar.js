e84ffe982635e9e4a343b33ff49df84d6320e5e8
github username <github email address>
2019-02-24 10:45:03 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,43 +3,46 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-    <NavLink
-      to="/"
-      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-      exact
-      /* add styling to Navlink */
-      style={link}
-      /* add prop for activeStyle */
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Home</NavLink> //end of home
-    <NavLink
-      to="/movies"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Movies</NavLink> // end of movies
-    <NavLink
-      to="/directors"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Directors</NavLink>
-    <NavLink
-      to="/actors"
-      exact
-      style={link}
-      activeStyle={{
-        background: 'darkblue'
-      }}
-    >Actors</NavLink>
-  </div>
+    <div className="navbar">
+  <NavLink to="/">Home</NavLink>
+  <NavLink to="/movies">Movies</NavLink>
+  <NavLink to="/directors">Directors</NavLink>
+  <NavLink to="/actors">Actors</NavLink>
+</div>
+    // <div className="navbar">
+    // <NavLink
+    //   to="/"
+    //   exact
+    //   style={link}
+    //   activeStyle={{
+    //     background: 'darkblue'
+    //   }}
+    // >Home</NavLink> //end of home
+    // <NavLink
+    //   to="/movies"
+    //   exact
+    //   style={link}
+    //   activeStyle={{
+    //     background: 'darkblue'
+    //   }}
+    // >Movies</NavLink> // end of movies
+    // <NavLink
+    //   to="/directors"
+    //   exact
+    //   style={link}
+    //   activeStyle={{
+    //     background: 'darkblue'
+    //   }}
+    // >Directors</NavLink>
+    // <NavLink
+    //   to="/actors"
+    //   exact
+    //   style={link}
+    //   activeStyle={{
+    //     background: 'darkblue'
+    //   }}
+    // >Actors</NavLink>
+//  </div>
 
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 
-ReactDOM.render((
-  <Router>
-    <React.Fragment>
-      <Navbar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/actors" component={Actors} />
-      <Route exact path="/directors" component={Directors} />
-      <Route exact path="/movies" component={Movies} />
-    </React.Fragment>
-  </Router>),
+render(
+  <App />,
   document.getElementById('root')
 );
