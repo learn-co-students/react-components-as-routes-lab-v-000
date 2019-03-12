d71953e92cbd0b71e0c8bd299ca964b8bb31028c
“PetyaDjanovska” <petya.djanovska@gmail.com>
2019-03-12 12:43:34 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <h1>Actors Page</h1>
+     {actors.map((actor) => 
+        <div key={actor.name} className='actor'>
+          <h3>{actor.name}</h3>
+          {actor.movies.join('; ')}
+          {/* {actor.movies.map(movie => 
+            <p>{movie}</p>
+            )} */}
+        </div>
+      )}
     </div>
   );
 };
