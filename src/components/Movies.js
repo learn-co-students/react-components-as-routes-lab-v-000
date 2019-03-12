8aa43e3a306b98dcd8dcadb6837a5f79f3da5371
Alexandra Fren <alexandra.fren@gmail.com>
2019-03-12 16:45:27 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+
+        {movies.map((movie, index) =>
+            <div key={movie.title}>
+            <br />
+            <h3>Title: {movie.title}</h3>
+            <p>Time: {movie.time} minutes</p>
+            <p>Genres: </p><ul>{movie.genres.map(genre => <li key={genre}>{genre}</li>)}</ul>
+            </div>
+          )}
+
     </div>
   );
 };
