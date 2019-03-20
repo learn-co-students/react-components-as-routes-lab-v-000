f2b027a758928540018124c966c7067f70390465
ConorHamilton19 <conhamil19@gmail.com>
2019-03-20 13:50:10 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route path="/" render={Home} />
+        <Route path="/movies" render={Movies} />
+        <Route path="/directors" render={Directors} />
+        <Route path="/actors" render={Actors} />
+      </div>
     </Router>
   );
 };
