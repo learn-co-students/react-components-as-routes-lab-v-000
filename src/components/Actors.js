a205243cd890162d5e3971f9aa5e8f597333dd01
github username <github email address>
2019-03-25 23:41:05 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const temp1 = actors
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div>
+          <h3>Name: {actor.name}</h3>
+          <h3>Movies: {
+              actor.movies.map(movie => (
+                <ul>
+                  <li>{movie}</li>
+                </ul>
+              ))
+            }</h3>
+        </div>) )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const temp = movies
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            <h2>Name: {movie.title}</h2>
+            <h4>Time: {movie.time}</h4>
+            <h4>Genres: </h4>
+            <ul>
+            {
+              movie.genres.map(genre => (
+                <li>{genre}</li>
+              ))
+            }
+            </ul>
+          </div>
+        ))
+
+        }
     </div>
   );
 };
