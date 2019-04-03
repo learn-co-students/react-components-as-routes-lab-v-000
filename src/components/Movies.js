36164a5216af3ad974baee6435bf7a03cf6dabd9
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-04-03 15:57:26 -0600

    Passed Tests

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>
+        Movies Page
+      </h1>
+      {movies.map(movie => {
+        return <div>
+          <h4>
+            {movie.title} - {movie.time} minutes
+          </h4>
+          <ul>
+            {movie.genres.map(genre =>    (<li>{genre}</li>)
+            )}
+          </ul>
+        </div>
+      })}
     </div>
   );
 };
