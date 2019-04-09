7cbea90fada94d61198c0f214d3f83697900bd22
github username <github email address>
2019-04-09 19:04:30 -0400

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Home Page</h1>
     </div>
   );
 };
 
 export default Home;
+
+// 1) should only render one <h1 /> inside of the <div />
+//    2) should render 'Home Page' inside of the <h1 />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,24 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className= "navbar">
+      <NavLink
+        to="/">Home</NavLink>
+        <NavLink
+          to="/movies">Movies</NavLink>
+          <NavLink
+            to="/actors">Actors</NavLink>
+            <NavLink
+              to="/directors">Directors</NavLink>
     </div>
   );
 };
 
 export default NavBar;
+
+// 1) wraps content in a div with .navbar class
+//  navlinks
+//    2) renders a Home <NavLink>
+//    3) renders a Movies <NavLink>
+//    4) renders an Actors <NavLink>
+//    5) renders a Directors <NavLink>
