847a686538811694bb2fb379b33edde2ee61629c
freeWillee <will.ik.lee@gmail.com>
2019-04-18 12:27:34 -0600

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
+
+  // const link = {
+  //   color: "white",
+  //   padding: "0.7em 0.84",
+  //   display: "inline-block",
+  //   border: "3px solid transparent",
+  //   position: "relative",
+  // }
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/movies">Movies</NavLink></li>
+        <li><NavLink to="/directors">Directors</NavLink></li>
+        <li><NavLink to="/actors">Actors</NavLink></li>
+      </ul>
     </div>
   );
 };
