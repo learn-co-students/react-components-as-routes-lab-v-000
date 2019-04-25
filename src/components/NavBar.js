b4b45aeab610c74f8f9b0f128efde9b02def21c2
Nadiafa <nadiafa581@gmail.com>
2019-04-25 15:45:38 +0100

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink to='/' exact>Home</NavLink>&nbsp;&nbsp;
+      <NavLink to='/actors' exact>Actors</NavLink>&nbsp;&nbsp;
+      <NavLink to='/directors' exact>Directors</NavLink>&nbsp;&nbsp;
+      <NavLink to='/movies' exact>Movies</NavLink>
     </div>
   );
 };
