a61f74f91bebb3361b07971107ff219799110f30
mattetress <mattetress@me.com>
2019-05-01 06:06:27 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => {
+        return (
+          <div key={director.name}>
+            <h3>Name: {director.name}</h3>
+            <ul>
+              {director.movies.map(movie => {
+                return (
+                  <li key={movie}>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+        return  <div key={movie.title}>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time}</p>
+
+              Genres:
+              <ul>
+                {movie.genres.map(genre => {
+                return  <li key={genre}>{genre}</li>
+                })}
+              </ul>
+
+
+          </div>
+        })}
     </div>
   );
 };
