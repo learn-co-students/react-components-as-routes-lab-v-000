bb3dfbaaf4c025c5f42adb423c7d806bb0612550
Aellon Krider <aellonkrider@gmail.com>
2019-05-03 13:43:55 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+        	return (
+        	<div key={movie.title}>
+        		<h3>{movie.title}</h3>
+        		<h6>Running Time: {movie.time} minutes</h6>
+        		<h5>Genres:</h5>
+        		<ul>
+      				{movie.genres.map(genre => {
+      					return <li key={genre}>{genre}</li>
+      				}
+      				)}
+      			</ul>
+        	</div>
+        	)
+        }
+        )}
     </div>
   );
 };
