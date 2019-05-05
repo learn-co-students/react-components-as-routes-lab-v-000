d62a966e376b9631828b8cff5e6e2c21b1c7d42b
Tahseen Siddiqui <tahseensiddiqui@Tahseens-MacBook-Pro.local>
2019-05-04 18:18:44 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link={
+  color: 'blue',
+  textDecoration: 'underline'
+}
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <NavLink to="/" exact style={link} activeStyle={{ color: 'red'}}>Home </NavLink>
+    <NavLink to="/movies" exact style={link} activeStyle={{ color: 'red'}}>Movies</NavLink>
+    <NavLink to="/directors" exact style={link} activeStyle={{ color: 'red'}}>Directors</NavLink>
+    <NavLink to="/actors" exact style={link} activeStyle={{ color: 'red'}}>Actors</NavLink>
     </div>
   );
 };
