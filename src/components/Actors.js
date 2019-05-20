2f82070e3ee0cb7e3360554efa2cb6381177e8a9
SallyZ12 <szasloff@gmail.com>
2019-05-20 15:49:50 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,7 +5,6 @@ const Actors = () => {
 
   return (
     <div>
-
     <h1> Actors Page </h1>
       {actors.map((actor, index) => (
         <div key = {index}>
@@ -17,7 +16,6 @@ const Actors = () => {
             </ul>
         </div>
       ))}
-
     </div>
   );
 };
