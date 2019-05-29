e15a08c09a05273879bbac9331f768a02a701721
David McGrann <dpmcgrann@gmail.com>
2019-05-29 12:36:12 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,8 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+
+      <h1>Directors Page</h1>
+
+        {directors.map((d, index) => (
+          <div key={index}>
+                <h3>Name: {d.name}</h3>
+                <p>Movies:</p>
+                <ul>
+                    {d.movies.map((movie, index) => (
+                        <li key={index}>{movie}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+      </div>
   );
 }
 
