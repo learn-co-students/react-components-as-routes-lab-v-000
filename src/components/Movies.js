c6642af3964e40f1593044656222d879217ef1f2
JustineGB <justinegayton@gmail.com>
2019-06-05 06:21:02 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,16 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div className="movie">
+      <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h2>Name: {movie.title}</h2>
+              <p>Time: {movie.time}</p>
+              <p>Genres: {movie.genres}</p>
+              <p>Metascore: {movie.metascore}</p>
+          </div>
+        ))}
     </div>
   );
 };
