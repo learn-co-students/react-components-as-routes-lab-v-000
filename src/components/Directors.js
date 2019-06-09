878f8dd4bc03861f425cb211b597823b155e8497
epic0448 <samuelogundiran@Yahoo.com>
2019-06-08 22:15:53 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,27 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+          <h1> Directors Page</h1>
+            {directors.map(function(director){
+              return(
+                 <div>
+                     <h1>{director.name} </h1>
+                     <p> Movies: </p>
+                    <ul>
+                           {director.movies.map(function(movie){
+                             return (
+                              <li>{movie}</li>
+                              )
+                             }
+                            )
+                         }  
+                    </ul>
+
+                  </div>
+                   ) 
+                  }
+                )
+               }
     </div>
   );
 }
