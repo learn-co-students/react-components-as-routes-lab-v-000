d23991d737bd15c4cb59610c6fe05140c8cb5e49
github username <github email address>
2019-06-19 13:48:42 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorList = () => {
+  return (
+    directors.map(director =>
+      <div key={director.name}>
+        {director.name}
+        <ul>
+          {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  )
+}
+
 const Directors = () => {
   return (
     <div>
+      <h1>Directors Page</h1>
+      {directorList()}
       {/*{code here}*/}
     </div>
   );
--- src/containers/App.js
+++ src/containers/App.js
@@ -13,6 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
+      <div>
+        <NavBar />
+        <Route exact path='/' component={ Home }/>
+        <Route exact path='/movies' component={ Movies }/>
+        <Route exact path='/directors' component={ Directors } />
+        <Route exact path='/actors' component={ Actors } />
+      </div>
       {/*{code here}*/}
     </Router>
   );
