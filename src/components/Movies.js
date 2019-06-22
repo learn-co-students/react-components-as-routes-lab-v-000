885a06c958d08bce7ebb72d1f6b184b5bd9f07ca
Marc Mills <mdmills1990@gmail.com>
2019-06-21 20:31:42 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+    <h1>Movies Page</h1>
+    {movies.map(movie => (
+      <div>
+        <h3>Name: {movie.title}</h3>
+        <p>Length: {movie.time} minutes</p>
+        <ol>
+          {movie.genres.map(genre => <i>{genre} </i>)}
+        </ol>
+      </div>
+    ))}
     </div>
   );
 };
