26691ca9a2c7b4ce8fbf55632898f4d57fb5b17f
indykiss <indirak@alumni.princeton.edu>
2019-06-24 08:33:04 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+// Indy, you are here. all set up
+// follow last code along to get this lab
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.name}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map(movie => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
