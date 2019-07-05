e239feccb376ee4cc8afad38fe3750c1633f3697
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-07-05 16:09:01 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,13 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Directors Page</h1>
+      {directors.map(director =>
+        <div key={director.id}>
+         <h3>Name: {director.name}</h3>
+         <p> Movies : {director.movies}</p>
+        </div>
+      )};
     </div>
   );
 }
