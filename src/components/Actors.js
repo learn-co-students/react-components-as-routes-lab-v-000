384362447f3ce248f519e905b79247bb147b0df5
Rachel Wyatt <rw126@evansville.edu>
2019-07-09 07:54:08 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        { actors.map((actor) => 
+        <div className = 'actor' >
+            <h2>{actor.name}</h2>
+            <h3>Movies: {actor.movies.map((name) =>
+              <ul> {name} </ul>)}</h3>
+        </div>
+        )}
     </div>
   );
 };
