27f43cb441f435c6a05854045526a2d621313717
Rachel Wyatt <rw126@evansville.edu>
2019-07-09 07:50:05 -0400

    add movies page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map((movie) => 
+        <div className = 'movie' >
+            <h2>{movie.title}</h2>
+            <h3>{movie.time} minutes</h3>
+            <h3>Genres: {movie.genres.map((genre) =>
+              <ul> {genre} </ul>)}</h3>
+        </div>
+        )}
     </div>
   );
 };
