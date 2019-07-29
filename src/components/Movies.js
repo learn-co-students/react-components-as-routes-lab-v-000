b528670cc66036af0f9013d67d19067df4bb7708
sarawall1187 <sarawall1187@gmail.com>
2019-07-29 09:46:29 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,16 +2,15 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = (movies) => {
- 
+
   return (
     <div>
       <h1>Movies Page</h1>
-      movies.map(movie => {
-            <div>{movie.title}</div>  
-    
-  }) 
-  </div>
-  )
-}
+      {movies.map((movie, index => {
+        <div>{movie.title}</div>  
+      }))}
+    </div>
+    )
+  }
 
 export default Movies;
