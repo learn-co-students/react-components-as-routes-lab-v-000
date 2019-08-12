81581abfa2f907ca0411f975e123761e9136b732
nmarini <nmarini44@gmail.com>
2019-08-11 18:54:20 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <div>
+            Name: {actor.name} <br />
+            Movies:
+            <ul>
+            {actor.movies.map((movie) => (
+              <li>{movie}</li>
+            ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
+
 
 export default Actors;
