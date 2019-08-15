784c1bf0704e505699e6af633919981dc97a5dca
Wendy Calderon <wendycalderon@gmail.com>
2019-08-14 22:02:59 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,12 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) =>
+        <div>Title: {movie.title}
+             Times: {movie.time}
+             Genres:<ul>{movie.genres.map((genre) => (<li>{genre}</li>))}</ul>
+        </div>)}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './containers/App';
+import Directors from './components/Directors';
 
 render (
   <App />,
+  document.getElementById('root')
+);
+
+// const Actors = () => <div><h1>Actors Page</h1></div>;
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/movies" component={Movies} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/actors" component={Actors} />
+    </React.Fragment>
+  </Router>),
   document.getElementById('root')
 );
