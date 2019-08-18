f55541cf5f9d998ce735a9a9b292694bb7691807
Ashley <ashleywong678@gmail.com>
2019-08-17 21:58:09 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,10 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      <div>
+        
+      </div>
     </div>
   );
 };
