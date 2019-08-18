473cf55b62508b4f459050713519482942480efa
Ashley <ashleywong678@gmail.com>
2019-08-18 10:42:47 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  // console.log(actors)
   return (
     <div>
       <h1>Actors Page</h1>
-      <div>
-        
-      </div>
+      {actors.map((actor, index) => (
+          <div key={index}>
+              <h3>Name: {actor.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                  {actor.movies.map((movie, index) => (
+                      <li key={index}>{movie}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  console.log(directors)
   return (
     <div>
       <h1>Directors Page</h1>
