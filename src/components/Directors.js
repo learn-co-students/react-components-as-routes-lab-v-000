66403c5060031f9a6857248b7c7bfeaaf0edbbfe
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-08-23 14:38:07 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { directors } from '../data';
 
+
+const Director = ({name, movies}) => {
+  return (
+    <div key={name}>
+      <h2>Name: {name}</h2>
+      <h3>Movies:</h3>
+      <ul>
+        {movies.map((movie, index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(Director)}
     </div>
   );
 }
