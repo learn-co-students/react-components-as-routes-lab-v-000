39a72ca11b30ae9c88f070355485704ecb55cc52
inkanimal <theatomproject@gmail.com>
2019-09-01 11:07:05 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { actors } from '../data';
 
+const Actor = ({name, movies}) => {
+  return (
+    <div> 
+      <h3>Name: {name}</h3>
+      <h3>Movies:</h3>
+      <ul>
+        {movies.map((movie, ind) => (
+          <li key={ind}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(Actor)}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+
+const Movie = ({title, time, genres}) => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div >
+    <h3>Title: {title}</h3>
+    <h4>Time: {time}</h4>
+    <h4>Genres:</h4>
+    <ul>{genres.map((genre, index) => (
+    <li> {genre}</li>))}
+    </ul>
+       
     </div>
-  );
+  )
+}
+
+return (
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map(Movie)}
+  </div>
+);
 };
 
 export default Movies;
