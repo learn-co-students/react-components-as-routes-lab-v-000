e073b3c08ba2fe57f4aa430446fec50f641a786f
Barbara Choy <barbchoy@gmail.com>
2019-09-07 16:52:45 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+
+      {movies.map(movie => (
+        <div>
+          {movie.title}
+          <ul>
+            <li>Time: {movie.time}</li>
+            <li>
+              Genre:{" "}
+              <ul>
+                {movie.genres.map(genre => (
+                  <li>{genre}</li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
