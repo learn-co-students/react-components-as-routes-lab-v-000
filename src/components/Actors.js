6d2f1d6b8aa0a8b2c60b60bc44ab420df021b4b8
christianavdm <christianavdm@gmail.com>
2019-09-18 07:44:50 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,13 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
+      { actors.map(actor => 
+        <div>
+        <h3>{actor.name}</h3>
+        <ul>
+        { actor.movies.map(movie => <li>{movie}</li>) }
+        </ul>
+        </div>) }
     </div>
   );
 };
