b8800bef91d2f79ccb042d48fbee21031eb3610a
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-22 17:41:25 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { actors } from '../data';
+//data coming from file, access using actors
 
 const Actors = () => {
+
+  const renderActors = () => {
+    return actors.map((actor, id) => {
+
+      const movies = actor.movies.map((movie, id) => {
+        return <li key={id}>{movie}</li>
+      })
+
+      return (
+        <div key={id}>
+        {actor.name}
+          <ul>
+            {movies}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
