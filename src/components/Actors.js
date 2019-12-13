6d255c644c02b384734c467f45b3075d51f4acfe
rgrier104 <rgrier104@gmail.com>
2019-12-13 12:28:01 -1000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+const renderActors = () => {
+  return actors.map(actor => {
+    return <div key={actor.name}>
+      {actor.name}
+      <ul>
+        {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  })
+}
+
+
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { directors } from '../data';
 
+const renderDirectors = () => {
+  return directors.map(director => {
+    return <div key={director.name}>
+      {director.name}
+      <ul>
+        {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  })
+}
+
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
