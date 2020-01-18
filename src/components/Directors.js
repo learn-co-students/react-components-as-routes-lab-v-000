6e609269aa10f4a08b8ef8081ea3f3a8238753c0
camneu37 <neuner.camille@gmail.com>
2020-01-17 19:00:01 -0600

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,20 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
+}
+
+const renderDirectors = () => {
+  return directors.map(d => (
+    <div>
+      <p>Director: {d.name}</p>
+      <p>Movies:</p>
+      <ul>{d.movies.map(m => (<li>{m}</li>))}</ul>
+    </div>
+  ))
 }
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
+
+const renderMovies = () => {
+  return movies.map(m => (
+    <div>
+      <p>Title: {m.title}</p>
+      <p>Time: {m.time}</p>
+      <p>Genres:</p>
+      <ul>{m.genres.map(g => (<li>{g}</li>))}</ul>
+    </div>
+  ))
+}
 
 export default Movies;
