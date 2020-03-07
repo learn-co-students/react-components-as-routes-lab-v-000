919b1d88868df2b9075fe9d4a7eecb0274293e1c
shktkt219 <shktkt219@gmail.com>
2020-03-07 11:22:07 +0200

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { actors } from '../data';
+import { act } from 'react-test-renderer';
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => 
+      <div>
+        <h3>Name: {actor.name}</h3>
+        <p>Movies:</p>
+        {actor.movies.map(movie => 
+        <ul>
+          <li>{movie}</li>
+        </ul>
+        )}
+      </div>
+      )}
     </div>
   );
 };
