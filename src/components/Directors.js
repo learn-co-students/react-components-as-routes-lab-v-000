ef054fe1c5e5701f82f54aa69cb3c17915e8df46
justindhall <hall.justin.d@gmail.com>
2020-05-27 15:04:07 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { directors } from '../data';
+import {directors} from '../data';
+
+const renderDirectors = () => {
+  return directors.map((director, index) => {
+    return (
+      <div key={index}>
+        <p>
+          {director.name}
+        </p>
+        <ul>
+          {renderMovies(director)}
+        </ul>
+      </div>
+    )
+  })
+};
+
+const renderMovies = director => {
+  return director.movies.map((movie, index) => {
+    return (
+      <li key={index}>
+        {movie}
+      </li>
+    )
+  })
+}
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>
+        Directors Page
+      </h1>
+      {renderDirectors()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { movies } from '../data';
+import {movies} from '../data';
+
+const renderMovies = () => {
+  return movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <p>
+          {movie.title}
+        </p>
+        <p>
+          {movie.time}
+        </p>
+        <ul>
+          {renderGenres(movie)}
+        </ul>
+      </div>
+    )
+  })
+};
+
+const renderGenres = movie => {
+  return movie.genres.map((genre, index) => {
+    return (
+      <li key={index}>
+        {genre}
+      </li>
+    )
+  })
+}
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>
+        Movies Page
+      </h1>
+      {renderMovies()}
     </div>
   );
 };
