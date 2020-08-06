f56f79b40bb32d8772992668598cd05007c0b790
sdcro <sdcrouse91@gmail.com>
2020-08-05 19:02:30 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,8 +7,8 @@ const NavBar = () => {
       <ul>
         <li><NavLink to="/" exact>Home</NavLink></li>
         <li><NavLink to="/movies" exact>Movies</NavLink></li>
-        <li><NavLink to="/directors" exact>Directors</NavLink></li>
         <li><NavLink to="/actors" exact>Actors</NavLink></li>
+        <li><NavLink to="/directors" exact>Directors</NavLink></li>
       </ul>
     </div>
   );
