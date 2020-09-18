33f1f536b92d66ec2d531a38959823c24e750139
Kat Tochowicz <ktochow1@binghamton.edu>
2020-09-18 18:24:49 +0200

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,10 +3,24 @@ import { directors } from '../data';
 
 const Directors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='director'>
+      <h1>Directors Page</h1>
+      {returnDirectors}
     </div>
   );
 }
+
+const returnDirectors = directors.map((director, i) => (
+  <div key={i}>
+    <h2>Name: {director.name}</h2>
+    <h3>Movies: </h3>
+    <ul>
+      {director.movies.map((movie, i) => (
+        <li key={i}>{movie}</li>
+      ))}
+    </ul>
+  </div>
+))
+
 
 export default Directors
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,22 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {returnMovies}
     </div>
   );
 };
+
+const returnMovies = movies.map((movie, i) => (
+  <div key={i}>
+    {movie.title} {movie.time}
+    <ul>Genres
+      {movie.genres.map((genre, i) => (
+        <li>{genre}</li>
+      ))}  
+    </ul> 
+  </div>
+))
+
 
 export default Movies;
