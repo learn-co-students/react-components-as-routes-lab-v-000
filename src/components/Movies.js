f0772d9ded197cf6d193406749940e79fc596b50
Olivia Tufte <ortufte@gmail.com>
2020-09-22 14:13:58 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => 
+        <div>
+          <h3>Title: {movie.title}</h3>
+          <h3>Time: {movie.time}</h3>
+          <h4>Genres:</h4>
+          <ul>
+            {movie.genres.map((genre) => 
+            <li>{genre}</li>
+            )}
+          </ul>
+        </div>)}
     </div>
   );
 };
