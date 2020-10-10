e26d183bf4add6cfdbe797f0ee3b5c788da268eb
Leslie Stevens <flymaven@protonmail.com>
2020-10-10 16:36:41 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,7 @@ const Movies = () => {
         <h1>Movies Page</h1>
         {movies.map((movie) => (
           <div>
-            <h3>Name: {movie.name}</h3>
+            <h3>Name: {movie.title}</h3>
             <h4>Time: {movie.time}</h4>
             <p>Genres:</p>
             <ul>
