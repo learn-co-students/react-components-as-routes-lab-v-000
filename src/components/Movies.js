d907cbe3164794ccbad0b2e474899fd21c58d31a
RonSala <ron.sala@gmail.com>
2021-02-18 15:21:57 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,17 +5,18 @@ const Movies = () => {
   return (
     <div>
       <h1>Movies Page</h1>
-      {console.log("movies:", movies)}
-      {movies.forEach((movie) => {
-        <div>
-          <h2>{movie.title}</h2>
-          <p></p>
-          <p></p>
-          <ul>
-
-          </ul>
-        </div>
-      })}
+       {movies.map((movie) => 
+        (<div>
+           <h2>{ movie.title }</h2>
+           <p>{ movie.time }</p>
+           <p>Genres:</p>
+           <ul>
+             {movie.genres.map((genre) =>
+               (<li>{ genre }</li>)
+             )}
+           </ul>
+         </div>)
+       )}
     </div>
   );
 };
