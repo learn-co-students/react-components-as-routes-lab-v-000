5906c02eca36ef86111f0c8efca841f014fd9d60
Mariel Hoepelman <marielhoepelman@gmail.com>
2021-02-20 15:08:50 -0500

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  const displayGenres = (movie) =>
+    movie.genres.map((genre) => <li>{genre}</li>);
+
+  const displayMovies = () =>
+    movies.map((movie) => (
+      <div>
+        <h3>Name: {movie.title}</h3>
+        <h4>Time: {movie.time}</h4>
+        <h4>Genres:</h4>
+        <ul>{displayGenres(movie)}</ul>
+      </div>
+    ));
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {displayMovies()}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import Home from "./Home";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const link = {
+    display: "block",
+  };
+
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to="/" style={link}>
+        Home
+      </NavLink>
+      <NavLink to="/actors" style={link}>
+        Actors
+      </NavLink>
+      <NavLink to="/movies" style={link}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={link}>
+        Directors
+      </NavLink>
     </div>
   );
 };
