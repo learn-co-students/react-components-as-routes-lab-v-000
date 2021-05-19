5ef68e7facc03398c3eca13749597f877bb5c467
MDAM182 <mdam2345@gmail.com>
2021-05-18 23:11:24 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import NavBar from '../components/NavBar';
+import Navbar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
 import Directors from '../components/Directors';
@@ -13,7 +13,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/actors" component={Actors} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/movies" component={Movies} />
+    </div>
     </Router>
   );
 };
