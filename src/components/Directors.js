8b5758d15086947960af51f2c5dbe2dd9ecb7ee3
emanao <emanao@me.com>
2021-05-24 11:07:20 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,15 +5,15 @@ const Directors = () => {
   return (
     <div>
         <h1>Directors Page</h1>
-      {directors.map((director, idx)=>(
-        <div key={idx}>{director.name}
+        {directors.map((director, idx)=>(
+            <div key={idx}>{director.name}
             <ul>
                 {director.movies.map((movie,idx)=>(
                 <li key={idx}>{movie}</li>
                 ))}
             </ul>
-        </div>
-          ))}
+            </div>
+        ))}
     </div>
   );
 }
