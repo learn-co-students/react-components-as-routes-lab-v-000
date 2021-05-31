8ffeb40b682a377c426ff873d55d3f112106ddc4
Cory Tveten <corytveten@gmail.com>
2021-05-30 20:51:57 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { movies } from '../data';
 
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map((movie, index) => (
+          <div key={index}>
+            <h3>{movie.title}</h3>
+            <p>Running Time: {movie.time}</p>
+            <p>Genres: {movie.genres.join(" ")}</p>
+            { (!!movie.metascore) ? <p>Metacritic Score: {movie.metascore}</p> : null}
+          </div>
+        ))}
     </div>
   );
 };
