eec3ca69744cd789d66cbbede834694566cfd871
Ian Gutwinski <iangutwinski@gmail.com>
2021-06-16 13:51:52 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  console.log(directors)
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(d => (
+        <div>
+          <p>{d.name}</p>
+        <ul>
+          {d.movies.map(m => (<li>{m}</li>))}
+        </ul>
+        </div>
+      ))}
     </div>
   );
 }
